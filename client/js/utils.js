3235d4cd572893c3d9704a328fa3ed62fcf8f3a2
Numan Kabir <numankabir100@gmail.com>
2026-03-29 23:04:31 -0700

    Fix API_URL to point to production Vercel site

--- client/js/utils.js
+++ client/js/utils.js
@@ -6,7 +6,7 @@
 // In development (localhost), use the local Express server.
 const API_URL = window.location.hostname === 'localhost'
   ? '/api'
-  : 'https://cs-portal-ij8faw1ze-numankabir100-2643s-projects.vercel.app/api';
+  : 'https://cs-portal-bck.vercel.app/api';
 
 /** Shorthand for getElementById */
 function id(x) { return document.getElementById(x); }
